0666985af5536f15be675bf2a230b2b7651367d5
BareeraSultan11 <bareerasultan96@gmail.com>
2026-04-28 01:59:46 +0500

    add task

--- To_di/todo_app/models.py
+++ To_di/todo_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Tasks(models.Model):
+  name = models.CharField(max_length=55)
+  descript = models.TextField()
+  date = models.DateField()
+  def __str__(self):
+    return self.name
+  
--- To_di/todo_app/views.py
+++ To_di/todo_app/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Tasks
 # Create your views here.
+def create_task(request):
+  if request.method=='POST':
+    # to get values from form
+    name = request.POST.get("name")
+    descript = request.POST.get("descript")
+    date = request.POST.get("date")
+    Tasks.objects.create(name=name, descript=descript, date=date)
+    return redirect("display")
+  return render(request, "todo_app/create.html")
